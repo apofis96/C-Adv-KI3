c414fee8e6d85226e50283185b38ec7d5bd65281
apofis96 <32576664+apofis96@users.noreply.github.com>
2017-12-20 12:45:26 +0200

    pluck

--- Chap3/Lab 4.1.5.1/Lab.cpp
+++ Chap3/Lab 4.1.5.1/Lab.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-
 int main()
 {
-	vector<int> v1;
-	v1.push_back(1);
-	v1.push_back(2);
-	v1.push_back(3);
-	vector<int> v2;
-	v2.push_back(5);
-	v2.push_back(2);
-	v2.push_back(6);
-	v2.push_back(4);
-  	for (auto it1 = v1.rbegin(); it1 != v1.rend(); ++it1)
-  	{
-    		for (auto it2 = v2.begin(); it2 != v2.end(); it2++)
-    		{
-			cout « *it1 + *it2 « " ";
-		}
-	cout « endl;
-	}
-return 0;
+  vector<int> values = { 2, 3, 2, 3, 1, 3};
+  int old_value;
+  cin >> old_value;
+  int new_value;
+  cin >> new_value;
+  replace(values.begin(), values.end(), old_value, new_value);
+  for (auto i : values)
+    cout << i << " ";
+  cout << endl;
+
+  return 0;
 }
