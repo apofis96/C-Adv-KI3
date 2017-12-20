8d0226abb12c1adc775912e8c0b481c7b97279cc
apofis96 <32576664+apofis96@users.noreply.github.com>
2017-12-20 02:59:32 +0200

    Chapter 1

--- Chap1/cpp_lab_1_3_6_1-A/lab.cpp
+++ Chap1/cpp_lab_1_3_6_1-A/lab.cpp
@@ -3,12 +3,19 @@
 
 using namespace std;
 
+
 int main()
 {
-	vector<int> v1{ 1, 2, 3 };
-	vector<int> v2{ 5, 2, 6, 4 };
-
-	for (auto it1 = v1.begin(); it1 != v1.end(); it1++)
+	vector<int> v1;
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	vector<int> v2;
+	v2.push_back(5);
+	v2.push_back(2);
+	v2.push_back(6);
+	v2.push_back(4);
+	for (auto it1 = v1.rbegin(); it1 != v1.rend(); ++it1)
 	{
 		for (auto it2 = v2.begin(); it2 != v2.end(); it2++)
 		{
@@ -16,6 +23,6 @@ int main()
 		}
 		cout << endl;
 	}
-
+	system("pause");
 	return 0;
 }
