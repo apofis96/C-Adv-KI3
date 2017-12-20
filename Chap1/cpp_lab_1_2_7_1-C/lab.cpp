6dfa85b36f3251be71577dc066e5059038444ee5
apofis96 <32576664+apofis96@users.noreply.github.com>
2017-12-20 02:50:05 +0200

    Chapter 1

--- Chap1/cpp_lab_1_2_7_1-C/lab.cpp
+++ Chap1/cpp_lab_1_2_7_1-C/lab.cpp
@@ -1,23 +1,60 @@
 ﻿#include <iostream>
-#include <list>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
+class Banknote
+{
+public: int value;
+		int number;
+		Banknote(int _val, int _number)
+		{
+			value = _val;
+			number = _number;
+		}
+};
+
+int GetMaxValue(vector<Banknote> b)
+{
+	int max = 0;
+	for(int i = 0; i < b.size(); i++)
+	{
+		max += b[i].number * b[i].value;
+	}
+	return max;
+}
+
 int main()
 {
-	int myints[] = { 1, 2,3 };
-	int yourints[] = { 5,8,6 };
-	list<int> elements1(myints, myints + 3);
-	list<int> elements2(yourints, yourints + 3);
-	cout << elements1.front() + elements2.front() << " ";
-	elements1.remove(2);
-	elements2.remove(8);
-	cout << elements1.back() + elements2.back() << " ";
-	elements1.pop_back();
-	elements2.pop_back();
-	elements2.insert(elements2.begin(), 7);
-	cout << elements1.back() + elements2.back() << " ";
-	elements2.pop_back();
-	cout << elements1.back() + elements2.back() << " ";
+	vector<Banknote> store;
+	store.push_back(Banknote(10, 5));
+	store.push_back(Banknote(20, 6));
+	store.push_back(Banknote(50, 3));
+	store.push_back(Banknote(100, 4));
+	store.push_back(Banknote(200, 5));
+	int sum1, sum2;
+	cin >> sum1;
+	cin >> sum2;
+
+	if(sum1 % 10 != 0) 
+		cout << "Incorrect amount of money. ";
+	else if(sum1 > GetMaxValue(store))
+	{
+		cout << "Not enough money. ";
+	}
+	else
+		cout << "Enough money. ";
+
+	if(sum2 % 10 != 0) 
+		cout << "Incorrect amount of money. " << endl;
+	else if(sum2 > GetMaxValue(store))
+	{
+		cout << "Not enough money. " << endl;
+	}
+	else
+		cout << "Enough money. " << endl;
+	system("pause");
 	return 0;
 }
